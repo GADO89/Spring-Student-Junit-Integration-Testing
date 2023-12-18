e7d9d882764f933b05a1a45a2757e02a483af3e3
GADO89 <gadomahmoud377@gmail.com>
2023-12-18 02:19:38 +0200

    createStudent IT valiate data

--- src/test/java/com/spring/testing/controller/StudentControllerIT.java
+++ src/test/java/com/spring/testing/controller/StudentControllerIT.java
@@ -1,7 +1,10 @@
 package com.spring.testing.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.testing.StudentApplicationIT;
 import com.spring.testing.model.Student;
+import com.spring.testing.model.StudentDto;
 import com.spring.testing.util.RequestUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,18 +17,25 @@ public class StudentControllerIT extends StudentApplicationIT {
 
     @Autowired
     private RequestUtil requestUtil;
-
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
-    public void createStudent_thenValidate(){
+    public void createStudent_thenValidate() throws JsonProcessingException {
         Student student=new Student();
         student.setName("Eslam");
         student.setAge(22);
         student.setPhone("01125589989");
         student.setActive(true);
-        ResponseEntity<Object> responseEntity=
-                requestUtil.post("api/create/",student,null, Object.class);
+        ResponseEntity<String> responseEntity=
+                requestUtil.post("api/create/",student,null, String.class);
         Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+        StudentDto studentDto=objectMapper.readValue(responseEntity.getBody(), StudentDto.class);
+        Assertions.assertEquals("Eslam",studentDto.getName());
+        Assertions.assertEquals(22,studentDto.getAge());
+        Assertions.assertEquals("01125589989",studentDto.getPhone());
+        Assertions.assertEquals(true,studentDto.isActive());
+
     }
     @Test
     public void getAllStudent_thenValidate(){
